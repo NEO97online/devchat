b4d9d03e3efe1018a67b046deddfe11ddbf49c77
michaelauderer <mike@silverstone.io>
2018-09-05 01:34:39 -0400

    created login and register endpoints with JWT

--- server/src/server.js
+++ server/src/server.js
@@ -7,13 +7,19 @@ import helmet from 'helmet'
 import basicAuth from 'express-basic-auth'
 import User from './models/User'
 import Message from './models/Message'
-
+import jwt from 'jsonwebtoken'
+import log from 'loglevel'
+import bodyParser from 'body-parser'
 
 const PORT = process.env.PORT || 4000
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+mongoose
+  .connect(
+    process.env.MONGODB_URL,
+    { useNewUrlParser: true }
+  )
   .then(() => {
-    console.log("Connected to MongoDB")
+    log.warn('Connected to MongoDB')
   })
   .catch(err => {
     console.error(err)
@@ -21,43 +27,27 @@ mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
 
 const app = express()
 app.use(helmet()) // Always wear a helment!
+app.use(bodyParser.json())
 const wss = expressWs(app).getWss()
 
 // app.ws is added by running expressWs(app) on line 14
 app.ws('/', (client, req) => {
-
   let displayName = 'anonymous'
 
   // need to try/catch because express-ws is catching our errors without logging them 😠
   try {
     client.send(`{ "connection": "ok" }`) // connection sucessful
     client.on('message', async content => {
-      console.log(`received: ${content}`)
+      log.warn(`received: ${content}`)
 
       // handle commands
       if (content[0] === '/') {
         const args = content.split(' ')
         const command = args.shift()
+        // Register <displayName> <email> <password>
         if (command === '/register') {
-          displayName = args[0]
-          // generate a unique tag
-          let tag
-          let currentTry = 0
-          while (currentTry < 100) {
-            tag = Math.floor(Math.random() * 9999).toString().padStart(4, "0")
-            const existingUser = await User.findOne({ displayName, tag }).exec()
-            if (!existingUser) {
-              break
-            }
-          }
-          const user = new User({
-            tag,
-            displayName,
-            password: process.env.SITELOCK_PASSWORD,            
-            email: "test@devchat.io"              
-          })
-          await user.save()
-          client.send(`Created account: ${displayName}#${tag}`)
+          const [displayName, email, password] = args
+          
         }
         return
       }
@@ -80,6 +70,42 @@ app.ws('/', (client, req) => {
   }
 })
 
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email }).exec()
+  if (user && (user.password === password)) {
+    const token = jwt.sign({ email: user.email }, process.env.JWT_KEY, { expiresIn: 14 * 8640000 /* 2 weeks */ })
+    res.send(token)
+    log.info("Sent JWT")
+  } else {
+    res.send('Invalid information. Please check username and password.')
+  }
+})
+
+app.post("/register", async (req, res) => {
+  const { displayName, email, password } = req.body
+  // generate a unique tag
+  let tag
+  let currentTry = 0
+  while (currentTry < 100) {
+    tag = Math.floor(Math.random() * 9999)
+      .toString()
+      .padStart(4, '0')
+    const existingUser = await User.findOne({ displayName, tag }).exec()
+    if (!existingUser) {
+      break
+    }
+  }
+  const user = new User({
+    tag,
+    displayName,
+    password,
+    email
+  })
+  await user.save()
+  res.send(`Created account: ${displayName}#${tag}`)
+})
+
 app.use(
   basicAuth({
     users: { dev: process.env.SITELOCK_PASSWORD },
@@ -89,4 +115,4 @@ app.use(
 
 app.use('/', express.static(path.join(__dirname, '../../client/build')))
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT} at ${new Date().toLocaleString()}`))
+app.listen(PORT, () => log.info(`Listening on port ${PORT} at ${new Date().toLocaleString()}`))
