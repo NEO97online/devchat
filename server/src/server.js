dcbc5cbfbcb0d1141fcd7a4f86f92cd9e2fea821
michaelauderer <mike@silverstone.io>
2018-09-27 22:30:20 -0400

    setup eslint

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,17 @@
-import {} from 'dotenv/config'
-import path from 'path'
-import express from 'express'
-import expressWs from 'express-ws'
-import mongoose from 'mongoose'
-import helmet from 'helmet'
-import basicAuth from 'express-basic-auth'
-import User from './models/User'
-import Message from './models/Message'
-import jwt from 'jsonwebtoken'
-import log from 'loglevel'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import bcrypt, { hash } from 'bcrypt'
+import {} from "dotenv/config"
+import path from "path"
+import express from "express"
+import expressWs from "express-ws"
+import mongoose from "mongoose"
+import helmet from "helmet"
+import basicAuth from "express-basic-auth"
+import User from "./models/User"
+import Message from "./models/Message"
+import jwt from "jsonwebtoken"
+import log from "loglevel"
+import bodyParser from "body-parser"
+import cors from "cors"
+import bcrypt, { hash } from "bcrypt"
 const saltRounds = 10
 
 const PORT = process.env.PORT || 4000
@@ -22,7 +22,7 @@ mongoose
     { useNewUrlParser: true }
   )
   .then(() => {
-    log.warn('Connected to MongoDB')
+    log.warn("Connected to MongoDB")
   })
   .catch(err => {
     console.error(err)
@@ -35,21 +35,21 @@ app.use(cors({ origin: true })) // for development purposes only, edit later
 const wss = expressWs(app).getWss()
 
 // app.ws is added by running expressWs(app) on line 14
-app.ws('/', (client, req) => {
-  let displayName = 'anonymous'
+app.ws("/", (client, req) => {
+  let displayName = "anonymous"
 
   // need to try/catch because express-ws is catching our errors without logging them 😠
   try {
-    client.send(`{ "connection": "ok" }`) // connection sucessful
-    client.on('message', async content => {
+    client.send("{ \"connection\": \"ok\" }") // connection sucessful
+    client.on("message", async content => {
       log.warn(`received: ${content}`)
 
       // handle commands
-      if (content[0] === '/') {
-        const args = content.split(' ')
+      if (content[0] === "/") {
+        const args = content.split(" ")
         const command = args.shift()
         // Register <displayName> <email> <password>
-        if (command === '/register') {
+        if (command === "/register") {
           const [displayName, email, password] = args
           
         }
@@ -81,12 +81,12 @@ app.post("/login", async (req, res) => {
   const verifiesHash = await bcrypt.compare(password, user.password)
   
   if (!user) {
-    res.status(403).send({ message: "This email isn't in use"})
+    res.status(403).send({ message: "This email isn't in use" })
     return
   }
 
   if (verifiesHash) {
-    const token = jwt.sign({ email: user.email }, process.env.JWT_KEY, { expiresIn: 14 * 8640000 /* 2 weeks */ })
+    const token = jwt.sign({ email: user.email }, process.env.JWT_KEY, { expiresIn: 14 * 8640000 })
     res.send(token)
     log.info("Sent JWT")
   } else {
@@ -106,11 +106,12 @@ app.post("/register", async (req, res) => {
   while (currentTry < 100) {
     tag = Math.floor(Math.random() * 9999)
       .toString()
-      .padStart(4, '0')
+      .padStart(4, "0")
     const existingUser = await User.findOne({ displayName, tag }).exec()
     if (!existingUser) {
       break
     }
+    currentTry++
   }
   const user = new User({
     tag,
@@ -130,6 +131,6 @@ app.use(
   })
 )
 
-app.use('/', express.static(path.join(__dirname, '../../client/build')))
+app.use("/", express.static(path.join(__dirname, "../../client/build")))
 
 app.listen(PORT, () => log.info(`Listening on port ${PORT} at ${new Date().toLocaleString()}`))
